79b6b6dd1baeb95fd92d03f5f257a476490f0c5b
Andrija <andrijagajicbusiness@gmail.com>
2021-05-02 09:32:51 +0200

    Added client pages and components

---
+++ client/src/pages/DrawingPage/DrawingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DrawingPage = () => {
+    return (
+        <div>
+            <p>Ovo ti je drawing page bajo moj</p>
+        </div>
+    )
+}
+
+export default DrawingPage;
---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <p>Ovo ti je home page bajo moj</p>
+        </div>
+    )
+}
+
+export default HomePage;
