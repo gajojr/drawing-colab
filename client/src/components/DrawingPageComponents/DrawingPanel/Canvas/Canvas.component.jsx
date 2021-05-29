a8a3750130afa9a0f003fd29d215e1d87500e93a
Andrija <andrijagajicbusiness@gmail.com>
2021-05-29 23:12:45 +0200

    started making canvas component

--- client/src/components/DrawingPageComponents/DrawingPanel/Canvas/Canvas.component.jsx
+++ client/src/components/DrawingPageComponents/DrawingPanel/Canvas/Canvas.component.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-import { Canvas, Info } from './Canvas.styles.js';
+import { Canvas } from './Canvas.styles.js';
 
 const DrawingCanvas = () => {
+    const canvasRef = useRef(null);
+    const contextRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+
+    useEffect(() => {
+        const width = document.getElementById('canvas').innerWidth;
+        const height = document.getElementById('canvas').innerHeight;
+
+        const canvas = canvasRef.current
+        canvas.width = width * 2;
+        canvas.height = height * 2;
+
+        canvas.style.width = `${width}px`;
+        canvas.style.height = `${height}px`;
+
+        const context = canvas.getContext('2d')
+        context.scale(2, 2);
+        context.lineCap = 'round';
+        context.strokeStyle = 'black';
+        context.lineWidth = 5;
+        contextRef.current = context;
+    }, []);
+
+    const startDrawing = ({ nativeEvent }) => {
+        const { offsetX, offsetY } = nativeEvent;
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+        setIsDrawing(true);
+    }
+
+    const finishDrawing = () => {
+        contextRef.current.closePath();
+        setIsDrawing(false);
+    }
+
+    const draw = ({ nativeEvent }) => {
+        if (!isDrawing) {
+            return;
+        }
+        const { offsetX, offsetY } = nativeEvent;
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke();
+    }
+
     return (
-        <Canvas>
-            <Info>Draw here</Info>
-        </Canvas>
+        <Canvas
+            id='canvas'
+            onMouseDown={startDrawing}
+            onMouseUp={finishDrawing}
+            onMouseMove={draw}
+            ref={canvasRef}
+        ></Canvas>
     )
 }
 
--- client/src/components/DrawingPageComponents/DrawingPanel/Canvas/Canvas.styles.js
+++ client/src/components/DrawingPageComponents/DrawingPanel/Canvas/Canvas.styles.js
@@ -1,17 +1,12 @@
 import styled from 'styled-components';
 
-export const Canvas = styled.div `
+export const Canvas = styled.canvas `
     width: 100%;
     height: 100%;
     display: flex;
     align-items: flex-start;
     justify-content: flex-start;
+    flex-direction: column;
     border: 3px solid #000;
     box-sizing: border-box;
-`;
-
-export const Info = styled.p `
-    background-color: gray;
-    padding: 3px;
-    margin-top: 0;
 `;
